82718fff011f2af0bd1d7ffd8ebcc631b61144d0
david travailleur <travailleur.david@gmail.com>
2020-12-14 19:15:50 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ garden_names = [
 ]
 
 garden_names.each do |garden_name|
-  garden_request = RestClient.get("https://source.unsplash.com/1200x700/?garden")
+  garden_request = RestClient.get('http://source.unsplash.com/1200x700/?garden')
   garden = Garden.new(
       name: garden_name,
       banner_url: garden_request.request.url
     )
   garden.save!
   3.times do
-    plant_request = RestClient.get("https://source.unsplash.com/400x300/?flower")
+    plant_request = RestClient.get("http://source.unsplash.com/400x300/?flower")
     plant = Plant.new(
       name: Faker::FunnyName.name,
       image_url: plant_request.request.url
